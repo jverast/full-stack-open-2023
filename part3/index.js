6437e8ed0b4acf700311bcf6aeb46e64b4d30593
jverast <jverast15@gmail.com>
2023-12-05 02:49:56 -0500

    part3 3.18

--- part3/index.js
+++ part3/index.js
@@ -9,29 +9,6 @@ const Person = require('./models/person')
 const app = express(),
   PORT = process.env.PORT || 3001
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456'
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523'
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345'
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendick',
-    number: '39-23-6423122'
-  }
-]
-
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(cors())
@@ -53,26 +30,24 @@ app.get('/api/persons', (req, res, next) => {
     .catch((err) => next(err))
 })
 
-app.get('/info', (req, res) => {
-  const total = persons.length,
-    date = new Date()
-
-  res.send(`
-    <p>Phonebook has info for ${total} people</p>
-    <p>${date}</p>
-  `)
+app.get('/info', (req, res, next) => {
+  Person.find({})
+    .countDocuments()
+    .then((total) => {
+      res.send(`
+        <p>Phonebook has info for ${total} people</p>
+        <p>${new Date()}</p>
+      `)
+    })
+    .catch((err) => next(err))
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id),
-    person = persons.find((person) => person.id === id)
-
-  if (!person) {
-    res.status(404).json({ error: 'not found' })
-    return
-  }
-
-  res.json(person)
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      person ? res.json(person) : res.status(404).json({ error: 'not found' })
+    })
+    .catch((err) => next(err))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
@@ -107,7 +82,6 @@ app.post('/api/persons', (req, res, next) => {
 
 app.put('/api/persons/:id', (req, res, next) => {
   const body = req.body
-  console.log(body)
 
   Person.findByIdAndUpdate(req.params.id, body, { new: true })
     .then((updatedPerson) => {
@@ -124,6 +98,11 @@ app.use(unknownEndpoint)
 
 const errorHandler = (err, req, res, next) => {
   console.error(err.message)
+
+  if (err.name === 'CastError') {
+    return res.status(400).json({ error: 'malformatted id' })
+  }
+
   next(err)
 }
 
