8fc035dbf40496eac5dc09e19f8263d188664c98
jverast <jverast15@gmail.com>
2023-12-08 22:48:04 -0500

    part4 4.6

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -20,8 +20,25 @@ const favoriteBlog = (blogs) => {
   }
 }
 
+const mostBlogs = (blogs) => {
+  if (!blogs.length) {
+    return {}
+  } else {
+    return Object.values(
+      blogs.reduce((authors, blog) => {
+        authors[blog.author] = authors[blog.author] || []
+        authors[blog.author].push({ author: blog.author })
+        return authors
+      }, {})
+    )
+      .map((item) => ({ author: item[0].author, blogs: item.length }))
+      .sort((a, b) => b.blogs - a.blogs)[0]
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
