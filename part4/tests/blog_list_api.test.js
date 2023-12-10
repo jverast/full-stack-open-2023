e3507685c23a7a0f717ca2a31665c2fd4b80147b
jverast <jverast15@gmail.com>
2023-12-09 23:44:22 -0500

    part4 4.10

--- part4/tests/blog_list_api.test.js
+++ part4/tests/blog_list_api.test.js
@@ -28,6 +28,24 @@ test('id is defined in each blog', async () => {
   response.body.forEach((blog) => expect(blog.id).toBeDefined())
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2
+  }
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogListDb()
+  expect(blogsAtEnd).toHaveLength(helper.blogListInitial.length + 1)
+  expect(response.body).toEqual({ ...newBlog, id: response.body.id })
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
