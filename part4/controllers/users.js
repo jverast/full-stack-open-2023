866ff1626292a84e445b3a028dd166db28d90708
jverast <jverast15@gmail.com>
2023-12-17 21:47:48 -0500

    part4 4.15

---
+++ part4/controllers/users.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
+usersRouter.post('/', async (request, response, next) => {
+  const { username, name, password } = request.body
+
+  const salt = 10,
+    passwordHash = await bcrypt.hash(password, salt)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash
+  })
+
+  const newUser = await user.save()
+  response.status(201).json(newUser)
+})
+
+module.exports = usersRouter
