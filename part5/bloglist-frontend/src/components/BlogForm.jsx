4d7a68426928d5529553b75bbe090f98947a6ab8
jverast <jverast15@gmail.com>
2023-12-22 16:14:19 -0500

    part5 5.8

--- part5/bloglist-frontend/src/components/BlogForm.jsx
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -15,40 +15,38 @@ const BlogForm = ({ createBlog }) => {
   }
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          title
-          <input
-            type="text"
-            name="title"
-            value={title}
-            onChange={({ target }) => setTitle(target.value)}
-          />
-        </div>
-        <div>
-          author
-          <input
-            type="text"
-            name="author"
-            value={author}
-            onChange={({ target }) => setAuthor(target.value)}
-          />
-        </div>
-        <div>
-          url
-          <input
-            type="text"
-            name="url"
-            value={url}
-            onChange={({ target }) => setUrl(target.value)}
-          />
-        </div>
-        <div>
-          <button type="submit">create</button>
-        </div>
-      </form>
-    </>
+    <form onSubmit={handleSubmit}>
+      <div>
+        title
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author
+        <input
+          type="text"
+          name="author"
+          value={author}
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url
+        <input
+          type="text"
+          name="url"
+          value={url}
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <div>
+        <button type="submit">create</button>
+      </div>
+    </form>
   )
 }
 
