5b35ba24db30365cb0163cfa576ecaf5370a8f36
jverast <jverast15@gmail.com>
2023-12-25 12:18:20 -0500

    part5 5.17

---
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -0,0 +1,13 @@
+describe('blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:5173')
+  })
+
+  it('login form is shown', function () {
+    cy.contains('Log in to application')
+    cy.contains('username')
+    cy.contains('password')
+    cy.get('button').should('contain', 'login')
+  })
+})
