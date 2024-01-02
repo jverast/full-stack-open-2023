2b8a7ff094cce8259b9d99c0e296f76974dc837d
jverast <jverast15@gmail.com>
2024-01-01 21:03:18 -0500

    part8 8.4

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -98,7 +98,7 @@ const typeDefs = `
   type Query {
     authorCount: Int!
     bookCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -107,7 +107,8 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: () => books,
+    allBooks: (root, args) =>
+      args.author ? books.filter((b) => b.author === args.author) : books,
     allAuthors: () => {
       const orderBooksByAuthor = books.reduce((auth, book) => {
         auth[book.author] ??= []
