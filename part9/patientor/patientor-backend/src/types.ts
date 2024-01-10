6120f02911b976bf5a5c75ad2001fe47f586e780
jverast <jverast15@gmail.com>
2024-01-10 01:22:21 -0500

    part9 9.22

--- part9/patientor/patientor-backend/src/types.ts
+++ part9/patientor/patientor-backend/src/types.ts
@@ -1,16 +1,60 @@
 export enum Gender {
-  male = 'male',
-  female = 'female',
-  other = 'other'
+  Male = 'male',
+  Female = 'female',
+  Other = 'other'
 }
 
-export interface Entry {}
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3
+}
 
 export interface Diagnose {
   code: string;
   name: string;
   latin?: string;
 }
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Patient {
   id: string;
@@ -21,6 +65,11 @@ export interface Patient {
   occupation: string;
   entries: Entry[];
 }
+
+// Define omit especial para uniones
+// type BetterOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// Define Entry sin la propiedad 'id'
+// type EntryWithoutId = BetterOmit<Entry, 'id'>;
 
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 export type NewPatient = Omit<Patient, 'id' | 'entries'>;
