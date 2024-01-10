88b38fedddc62639c350830cedeb623f8954e3de
jverast <jverast15@gmail.com>
2024-01-09 23:20:01 -0500

    part9 9.21

---
+++ part9/patientor/patientor-frontend/src/components/Patient/index.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { Patient } from '../../types';
+import { useParams } from 'react-router-dom';
+
+import patientService from '../../services/patients';
+
+import FemaleIcon from '@mui/icons-material/Female';
+import MaleIcon from '@mui/icons-material/Male';
+
+const PatientDetails = () => {
+  const [patient, setPatient] = useState<Patient | null>(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (!id) return;
+
+    const fetchPatient = async (): Promise<void> => {
+      const patient = await patientService.getEntry(id);
+      setPatient(patient);
+    };
+
+    fetchPatient();
+  }, [id]);
+
+  if (!patient) return null;
+
+  return (
+    <>
+      <div>
+        <h2>
+          {patient.name}{' '}
+          {patient.gender === 'male' ? <MaleIcon /> : <FemaleIcon />}
+        </h2>
+      </div>
+      <div>
+        <div>ssh: {patient.ssn}</div>
+        <div>occupation: {patient.occupation}</div>
+      </div>
+    </>
+  );
+};
+
+export default PatientDetails;
